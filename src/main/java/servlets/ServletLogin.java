76c20a97767e9b7cb4fb02d055127707450cb749
Samuel <samugit.projects@gmail.com>
2023-10-28 13:05:12 -0300

    Aula22_12 Front-End e Back-End dentro do projeto web

--- src/main/java/servlets/ServletLogin.java
+++ src/main/java/servlets/ServletLogin.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+/*O chamado Controller são as servlets ou ServletLoginController*/
 @WebServlet("/ServletLogin") // mapeamento de url que vem da tela
 public class ServletLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
