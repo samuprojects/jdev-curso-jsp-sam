8b7d3ef165c7be9efaf41d7246033fbfd5263e25
Samuel <samugit.projects@gmail.com>
2024-05-01 01:30:24 -0300

    Aula22_34 Criando o SQL, método e DAO para salvar no banco de dados

--- src/main/java/servlets/ServletUsuarioController.java
+++ src/main/java/servlets/ServletUsuarioController.java
@@ -1,18 +1,20 @@
 package servlets;
 
+import java.io.IOException;
+
+import dao.DAOUsuarioRepository;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.ModelLogin;
 
-import java.io.IOException;
-
 
 public class ServletUsuarioController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private DAOUsuarioRepository daoUsuarioRepository = new DAOUsuarioRepository();
        
 
     public ServletUsuarioController() {
@@ -23,23 +25,32 @@ public class ServletUsuarioController extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		String id = request.getParameter("id");
-		String nome = request.getParameter("nome");
-		String email = request.getParameter("email");
-		String login = request.getParameter("login");
-		String senha = request.getParameter("senha");
-		
-		ModelLogin modelLogin = new ModelLogin();
-		modelLogin.setId(id != null && !id.isEmpty() ? Long.parseLong(id) : null);
-		modelLogin.setNome(nome);
-		modelLogin.setEmail(email);
-		modelLogin.setLogin(login);
-		modelLogin.setSenha(senha);
-		
-		request.setAttribute("modelLogin", modelLogin);
-		
-		request.getRequestDispatcher("principal/usuario.jsp").forward(request, response);
-
+		try {
+			String id = request.getParameter("id");
+			String nome = request.getParameter("nome");
+			String email = request.getParameter("email");
+			String login = request.getParameter("login");
+			String senha = request.getParameter("senha");
+			
+			ModelLogin modelLogin = new ModelLogin();
+			
+			modelLogin.setId(id != null && !id.isEmpty() ? Long.parseLong(id) : null);
+			modelLogin.setNome(nome);
+			modelLogin.setEmail(email);
+			modelLogin.setLogin(login);
+			modelLogin.setSenha(senha);
+			
+			daoUsuarioRepository.gravarUsuario(modelLogin);
+			
+			request.setAttribute("msg", "Operação realizada com sucesso!");			
+			request.setAttribute("modelLogin", modelLogin);		
+			request.getRequestDispatcher("principal/usuario.jsp").forward(request, response);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			RequestDispatcher redirecionar = request.getRequestDispatcher("erro.jsp");
+			request.setAttribute("msg", e.getMessage());
+			redirecionar.forward(request, response);
+			}
+		}
 	}
-
-}
--- src/main/webapp/principal/usuario.jsp
+++ src/main/webapp/principal/usuario.jsp
@@ -83,6 +83,12 @@
 															<button class="btn btn-info waves-effect waves-light">Excluir</button>
 
 														</form>
+														</div>
+														</div>
+														</div>
+														</div>
+														<span>${msg}</span>
+														
 													</div>
 													<!-- Page-body end -->
 												</div>
